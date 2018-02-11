c5b3b645a25a6f67067ddab19aec54dff7648320
zk <wingfayfree@qq.com>
2018-02-11 16:27:12 +0800

    添加项目文件。

---
+++ ZK.NetCore.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZK.NetCore.Util;
+
+namespace ZK.NetCore.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int iteration = 100 * 1000;
+
+            string s = "";
+            CodeTimer.Time("String Concat", iteration, () => { s += "a"; });
+
+            StringBuilder sb = new StringBuilder();
+            CodeTimer.Time("StringBuilder", iteration, () => { sb.Append("a"); });
+
+
+
+        }
+    }
+}
