7379b8bc1fefbc37fa2cb90af5ca2fc2aa3d751b
zk <wingfayfree@qq.com>
2018-03-22 14:47:14 +0800

    测试读取appseting.json 成为list

--- ZK.NetCore.ConsoleApp/Program.cs
+++ ZK.NetCore.ConsoleApp/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using ZK.NetCore.Util;
+using Amqp;
+using Amqp.Framing;
+using System.Threading;
 
 namespace ZK.NetCore.ConsoleApp
 {
@@ -11,16 +14,173 @@ namespace ZK.NetCore.ConsoleApp
         {
             //Console.WriteLine("Hello World!");
 
-            int iteration = 100 * 1000;
+            //int iteration = 100 * 1000;
 
-            string s = "";
-            CodeTimer.Time("String Concat", iteration, () => { s += "a"; });
+            //string s = "";
+            //CodeTimer.Time("String Concat", iteration, () => { s += "a"; });
 
-            StringBuilder sb = new StringBuilder();
-            CodeTimer.Time("StringBuilder", iteration, () => { sb.Append("a"); });
+            //StringBuilder sb = new StringBuilder();
+            //CodeTimer.Time("StringBuilder", iteration, () => { sb.Append("a"); });
 
 
+            string address = "amqp://guest:guest@127.0.0.1:5672";
+            if (args.Length > 0)
+            {
+                address = args[0];
+            }
 
+            // uncomment the following to write frame traces
+            //Trace.TraceLevel = TraceLevel.Frame;
+            //Trace.TraceListener = (l, f, a) => Console.WriteLine(DateTime.Now.ToString("[hh:mm:ss.fff]") + " " + string.Format(f, a));
+
+            Console.WriteLine("Running request client...");
+            new Client(address).Run();
+
+        }
+
+        class Client
+        {
+            readonly string address;
+            string replyTo;
+            Connection connection;
+            Session session;
+            ReceiverLink receiver;
+            SenderLink sender;
+            int offset;
+
+            public Client(string address)
+            {
+                this.address = address;
+                this.replyTo = "client-" + Guid.NewGuid().ToString();
+            }
+
+            public void Run()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        this.Cleanup();
+                        this.Setup();
+
+                        this.RunOnce();
+
+                        this.Cleanup();
+                        break;
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine("Reconnect on exception: " + exception.Message);
+
+                        Thread.Sleep(5000);
+                    }
+                }
+            }
+
+            void Setup()
+            {
+                this.connection = new Connection(new Address(address));
+                this.session = new Session(connection);
+
+                Attach recvAttach = new Attach()
+                {
+                    Source = new Source() { Address = "request_processor" },
+                    Target = new Target() { Address = this.replyTo }
+                };
+
+                this.receiver = new ReceiverLink(session, "request-client-receiver", recvAttach, null);
+                this.receiver.Start(300);
+                this.sender = new SenderLink(session, "request-client-sender", "request_processor");
+            }
+
+            void Cleanup()
+            {
+                var temp = Interlocked.Exchange(ref this.connection, null);
+                if (temp != null)
+                {
+                    temp.Close();
+                }
+            }
+
+            void RunOnce()
+            {
+                Message request = new Message("hello " + this.offset);
+                request.Properties = new Properties() { MessageId = "command-request", ReplyTo = this.replyTo };
+                request.ApplicationProperties = new ApplicationProperties();
+                request.ApplicationProperties["offset"] = this.offset;
+                sender.Send(request, null, null);
+                Console.WriteLine("Sent request {0} body {1}", request.Properties, request.Body);
+
+                while (true)
+                {
+                    Message response = receiver.Receive();
+                    receiver.Accept(response);
+                    Console.WriteLine("Received response: {0} body {1}", response.Properties, response.Body);
+
+                    if (string.Equals("done", response.Body))
+                    {
+                        break;
+                    }
+
+                    this.offset = (int)response.ApplicationProperties["offset"];
+                }
+            }
+        }
+
+        /// <summary>
+        /// This example assumes a topic and a subscirption named "sub1" is precreated.
+        /// Example.Entity should be set to the topic name.
+        /// </summary>
+        class TopicExample : Example
+        {
+            public override void Run()
+            {
+                this.SendReceiveAsync(10).GetAwaiter().GetResult();
+            }
+
+            async Task SendReceiveAsync(int count)
+            {
+                Trace.WriteLine(TraceLevel.Information, "Establishing a connection...");
+                Connection connection = await Connection.Factory.CreateAsync(this.GetAddress());
+
+                Trace.WriteLine(TraceLevel.Information, "Creating a session...");
+                Session session = new Session(connection);
+
+                Trace.WriteLine(TraceLevel.Information, "Creating a sender link...");
+                SenderLink sender = new SenderLink(session, "topic-sender-link", this.Entity);
+
+                Trace.WriteLine(TraceLevel.Information, "Sending {0} messages...", count);
+                for (int i = 0; i < count; i++)
+                {
+                    Message message = new Message();
+                    message.Properties = new Properties() { MessageId = "topic-test-" + i };
+                    message.BodySection = new Data() { Binary = Encoding.UTF8.GetBytes("message #" + i) };
+                    await sender.SendAsync(message);
+                }
+
+                Trace.WriteLine(TraceLevel.Information, "Closing sender...");
+                await sender.CloseAsync();
+
+                Trace.WriteLine(TraceLevel.Information, "Receiving messages from subscription...");
+                ReceiverLink receiver = new ReceiverLink(session, "receiver-link", this.Entity + "/Subscriptions/sub1");
+                for (int i = 0; i < count; i++)
+                {
+                    Message message = await receiver.ReceiveAsync();
+                    if (message == null)
+                    {
+                        break;
+                    }
+
+                    receiver.Accept(message);
+                }
+
+                Trace.WriteLine(TraceLevel.Information, "Closing receiver...");
+                await receiver.CloseAsync();
+
+                Trace.WriteLine(TraceLevel.Information, "Shutting down...");
+                await session.CloseAsync();
+                await connection.CloseAsync();
+            }
         }
     }
 }
--- ZK.NetCore.WebAPI/Controllers/ValuesController.cs
+++ ZK.NetCore.WebAPI/Controllers/ValuesController.cs
@@ -1,19 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace ZK.NetCore.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+
+        public IConfiguration Configuration { get; internal set; }
+        public ValuesController()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<(string, string, string)> Get()
         {
-            return new string[] { "value1", "value2" };
+
+            var Sections = Configuration.GetSection("AppSettings").GetChildren();
+
+
+            List<(string, string, string)> list = new List<(string, string, string)>();
+
+            foreach(IConfigurationSection section in Sections)
+            {
+                string strKey = section.Key;
+
+
+                foreach(IConfigurationSection sectionChild in section.GetChildren())
+                {
+                    list.Add((strKey, sectionChild.Key, sectionChild.Value));
+                }
+
+                
+            }
+
+            var data = Configuration.GetSection("AppSettings");
+
+            var liste = from key in data.GetChildren()
+                        from sectionChild in data.GetSection(key.Key).GetChildren()
+                        select (key.Key, sectionChild.Key, sectionChild.Value);
+
+
+
+
+            //var pairs =
+            //    from key in Section.Cast<string>()
+            //    from value in Section
+            //    //select new Object { 
+
+            //    //     key.Split(':')[0],
+            //    //    key.Split(':')[1],
+            //    //     value
+            //    //}
+
+            //if (pairs == null)
+            //{
+            //    return new List<T>();
+            //}
+
+            //return pairs.ToList();
+
+            //return new string[] { "value1", "value2" };
+
+            return liste.ToList(); ;
         }
 
         // GET api/values/5
