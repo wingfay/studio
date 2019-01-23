f5f88dfaa6110e245d2d00e5b8ca786f71110992
wingfay <wingfay@gmail.com>
2019-01-23 16:52:32 +0800

    加入 SignalR  Sample项目,聊天测试项目。

---
+++ SignalRSimple/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(SignalRSimple.Startup), "Configuration")]
+namespace SignalRSimple
+{
+      public class Startup
+      {
+         public void Configuration(IAppBuilder app)
+         {
+            Microsoft.AspNet.SignalR.StockTicker.Startup.ConfigureSignalR(app);
+         }
+      }
+   }
